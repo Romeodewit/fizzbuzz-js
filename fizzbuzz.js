8402b07a6be35fecd070e54a3484a666d2b803ff
Romeodewit <romeo@classicremedies.co.uk>
2021-02-17 13:50:58 +0000

    cheers

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,12 +1,22 @@
-
-
+const counter = () => {
+    for(let i = 0; i <= 100; i++) {
+        if(i % 3 === 0 && i % 5 === 0) {
+            console.log(i + " fizzbuzz")
+        } else if (i % 3 === 0) {
+            console.log(i + " fizz")
+        } else if (i % 5 === 0) {
+            console.log(i + " buzz")
+        } else {
+            console.log(i)
+        }
+    }
+}
 // Here, we create our main function.
 function fizzbuzz() {
     
     console.log('Welcome to FizzBuzz!');
 
-    // Put your code here...
-
+    counter();
     
 }
 
